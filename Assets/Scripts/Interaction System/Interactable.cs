b5d7902f8f7b73cc77bf8682d79e4230a495e804
RezaKusha <reza.kusha1@gmail.com>
2023-10-22 08:56:08 +0330

    New Interaction System & Setting Script Added, Old Scripts Removed

---
+++ Assets/Scripts/Interaction System/Interactable.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Settings;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace InteractionSystem
+{
+    public class Interactable : MonoBehaviour
+    {
+        [SerializeField] private BoxCollider interationArea;
+
+        public UnityEvent onInteraction;
+
+        private bool canBeInteracted;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+                canBeInteracted = true;
+        }
+
+        private void Break()
+        {
+            // dsdas
+        }
+
+        private void Update()
+        {
+            //Interactable glass = new Interactable();
+            //glass.onInteraction.AddListener(Break);
+            if (canBeInteracted == false)
+                return;
+
+            if (Input.GetKeyDown(Keybindings.InteractionKey))
+                onInteraction.Invoke();
+        }
+
+        private void Reset()
+        {
+            interationArea = GetComponent<BoxCollider>();
+            if (interationArea == null)
+                interationArea = gameObject.AddComponent<BoxCollider>();
+        }
+    }
+}
