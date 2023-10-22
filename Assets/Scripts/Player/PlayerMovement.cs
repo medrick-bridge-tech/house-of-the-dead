ad14799e73a3365c6ca746a002c5b5ab437639d7
Tara Karimi <tara.karimi@icloud.com>
2023-10-22 14:19:57 +0330

    Fix folder structure for player scripts

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float walkSpeed = 2f;
+    [SerializeField] private float runSpeed = 3.0f;
+    [SerializeField] private float rotationSpeed = 7f;
+    [SerializeField] private Joystick joystick;
+    private Rigidbody rb;
+    private float joystickRange = 0.5f;
+    private float joystickMagnitude;
+    private float currentSpeed;
+    private float horizontal, vertical;
+    
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        horizontal = joystick.Vertical;
+        vertical = joystick.Horizontal;
+
+        Vector3 direction = new Vector3(-horizontal, 0f, vertical).normalized;
+
+        if (rb != null)
+        {
+            CalculateSpeed();
+
+            rb.MovePosition(rb.position + direction * currentSpeed * Time.deltaTime);
+
+            if (direction != Vector3.zero)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.up);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            }
+        }
+    }
+
+    private void CalculateSpeed()
+    {
+        joystickMagnitude = Mathf.Max(Mathf.Abs(vertical), Mathf.Abs(horizontal));
+        currentSpeed = joystickMagnitude > 0.01f ? (joystickMagnitude < joystickRange ? walkSpeed : runSpeed) : 0;
+    }
+
+    public string ReturnState()
+    {
+        return currentSpeed > 0 ? (joystickMagnitude < joystickRange ? "Walk" : "Run") : "Idle";
+    }
+}
