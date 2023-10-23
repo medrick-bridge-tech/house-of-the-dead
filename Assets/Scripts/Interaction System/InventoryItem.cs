1789f1a2f11ffab3ed3ddc60b5224fc014b7a5f3
RezaKusha <reza.kusha1@gmail.com>
2023-10-23 14:26:21 +0330

    Create inventory system scripts and scriptable object

---
+++ Assets/Scripts/Interaction System/InventoryItem.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class InventoryItem : Interactable
+    {
+        [SerializeField] private ItemData itemData;
+
+        private void Awake()
+        {
+            onInteraction.AddListener(AddToInventory);
+        }
+
+        private void AddToInventory(InventorySystemCharacter interactor)
+        {
+            var inventory = interactor.Inventory;
+            if (inventory != null)
+                inventory.AddItem(itemData);
+        }
+    }
---
+++ Assets/Scripts/Model/Inventory.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory
+{
+    private Dictionary<string, int> items = new Dictionary<string, int>();
+
+    public void AddItem(ItemData itemData)
+    {
+        string itemName = itemData.itemName;
+        if (items.ContainsKey(itemName))
+        {
+            Debug.Log("ADD " + itemData.name);
+            items[itemName]++;
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    public void RemoveItem(ItemData itemData)
+    {
+        string itemName = itemData.itemName;
+        if (items.ContainsKey(itemName))
+        {
+            if (items[itemName] > 1)
+            {
+                items[itemName]--;
+            }
+            else
+            {
+                items.Remove(itemName);
+            }
+        }
+    }
+}
