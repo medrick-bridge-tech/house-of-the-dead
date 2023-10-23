a8eec2b06ebc21d14c79e8902439ba52d13e0de3
RezaKusha <reza.kusha1@gmail.com>
2023-10-23 14:26:21 +0330

    Create inventory system scripts and scriptable object
    (cherry picked from commit 1789f1a2f11ffab3ed3ddc60b5224fc014b7a5f3)

---
+++ Assets/Scripts/Character/Character.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Character : MonoBehaviour, InventorySystemCharacter
+{
+    private Inventory _inventory;
+
+    public Inventory Inventory => _inventory;
+
+    private void Awake()
+    {
+        _inventory = new Inventory();
+    }
+}
