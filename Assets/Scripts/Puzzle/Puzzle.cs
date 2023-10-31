46b78760457e31e514af521534049807b840845c
Tara Karimi <tara.karimi@icloud.com>
2023-10-31 12:08:56 +0330

    Implement the handle character interaction method in puzzle and numlock

--- Assets/Scripts/Puzzle/Puzzle.cs
+++ Assets/Scripts/Puzzle/Puzzle.cs
@@ -1,9 +1,22 @@
 using System;
+using Cinemachine;
 using UnityEngine;
 
-public abstract class Puzzle : MonoBehaviour
+public abstract class Puzzle : Interactable
 {
+    [SerializeField] public CinemachineVirtualCamera virtualCamera;
     public Action OnPuzzleSolved, OnPuzzleFailed;
-    
+
     public abstract void HandlePuzzleFinish();
+
+    protected virtual void Awake()
+    {
+        onInteraction.AddListener(HandleCharacterInteraction);
+    }
+
+    protected virtual void HandleCharacterInteraction(Character character)
+    {
+        character.PuzzleSolveMode(virtualCamera);
+    }
+
 }
