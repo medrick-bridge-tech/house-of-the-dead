859dd480de55a82d1b6f3af0dcecb083cd27ab41
Tara Karimi <tara.karimi@icloud.com>
2023-10-31 12:05:18 +0330

    Add methods to character class to handle puzzle mode

--- Assets/Scripts/Inventory/Character.cs
+++ Assets/Scripts/Inventory/Character.cs
@@ -1,7 +1,11 @@
-﻿using UnityEngine;
+﻿using Cinemachine;
+using UnityEngine;
 
 public class Character : MonoBehaviour, InventorySystemCharacter
 {
+    [SerializeField] private GameObject joystick, backButton, openButton;
+    
+    private CameraSwitcher cameraSwitcher;
     private Inventory _inventory;
 
     public Inventory Inventory => _inventory;
@@ -9,5 +13,22 @@ public class Character : MonoBehaviour, InventorySystemCharacter
     private void Awake()
     {
         _inventory = new Inventory();
+        cameraSwitcher = GetComponent<CameraSwitcher>();
     }
+
+    public void PuzzleSolveMode(CinemachineVirtualCamera virtualCamera)
+    {
+        cameraSwitcher.SwitchToPuzzleCamera(virtualCamera);
+        joystick.SetActive(false);
+        backButton.SetActive(true);
+    }
+    
+    public void DefocusFromPuzzle()
+    {
+        cameraSwitcher.DefocusPuzzleCamera();
+        joystick.SetActive(true);
+        backButton.SetActive(false);
+        openButton.SetActive(false);
+    }
+
 }
