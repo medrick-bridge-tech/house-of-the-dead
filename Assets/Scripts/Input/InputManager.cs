ef99f96ff622aa568c3b52d54e41fd8cf0d0910a
Tara Karimi <tara.karimi@icloud.com>
2023-11-01 16:02:22 +0330

    Add interaction button and its functionality in Input Manager

--- Assets/Scripts/Input/InputManager.cs
+++ Assets/Scripts/Input/InputManager.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InputManager : MonoBehaviour
 {
+    [SerializeField] private Button interactionButton;
+    [SerializeField] private Sprite pickUpIcon, puzzleIcon;
     private static InputManager instance;
-    
+    private Image buttonImage;
+    public bool interactionKeyPressed { get; private set; }
     public static InputManager Instance
     {
         get
@@ -27,6 +31,9 @@ public class InputManager : MonoBehaviour
         }
         else
             Destroy(gameObject);
+        
+        interactionButton.onClick.AddListener(InteractionButtonClicked);
+        buttonImage = interactionButton.transform.GetChild(0).GetComponent<Image>();
     }
 
     public bool IsTouching()
@@ -37,5 +44,22 @@ public class InputManager : MonoBehaviour
     public bool IsClicking()
     {
         return Input.GetMouseButtonDown(0);
+    }
+
+    public void InteractionButtonClicked()
+    {
+        interactionKeyPressed = true;
+    }
+
+    public void ActivateInteractionButton(bool isPickable)
+    {
+        buttonImage.sprite = isPickable ? pickUpIcon : puzzleIcon;
+        interactionButton.gameObject.SetActive(true);
+    }
+    
+    public void DeActivateInteractionButton()
+    {
+        interactionKeyPressed = false;
+        interactionButton.gameObject.SetActive(false);
     }
 }
