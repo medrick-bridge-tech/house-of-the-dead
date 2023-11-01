c6e1cf30f7de4469a5bc7d575a1cdea7101eb0a6
-Arman.Aghighi <armanaghighi@gmail.com>
2023-11-01 10:40:51 +0330

    Done Request Changes

--- Assets/Scripts/Puzzle/ItemPuzzle.cs
+++ Assets/Scripts/Puzzle/ItemPuzzle.cs
@@ -5,18 +5,21 @@ public class ItemPuzzle : Puzzle
 {
     private string _selectedObject;
     private bool _isSolved = false;
+    private AudioSource _listener;
     private Inventory _myInventory;
+    private Character _character;
 
-    [SerializeField] private List<ItemData> requiredItems;
-    [SerializeField] private Character _characterInventory;
+    [SerializeField] private List<ItemData> _requiredItems;
     [SerializeField] private InventoryUI _inventoryUI;
-    [SerializeField] private Animator _solveAnimator;
-    
+    [SerializeField] private Animator _onSolveAnimation;
+    [SerializeField] private Animator _onStartPuzzleAnimation;
 
-    private void Start()
+    protected override void Awake()
     {
+        base.Awake();
         OnPuzzleSolved += OpenDoor;
-        _myInventory = _characterInventory.Inventory;  
+        _listener = gameObject.GetComponent<AudioSource>();
+        _character = GameObject.Find("Player").GetComponent<Character>();     
     }    
 
     public override void HandlePuzzleFinish()
@@ -27,33 +30,43 @@ public class ItemPuzzle : Puzzle
     
     private void OpenDoor()
     {
-        _solveAnimator.SetTrigger("RoomOneTrigger");
+        _onSolveAnimation.SetTrigger("RoomOneTrigger");
+        _listener.Play();
+        _onStartPuzzleAnimation.SetBool("OpenDoor", false);
     }
 
-    private void Update()
+    protected override void Update()
     {
+        base.Update();
         if (!_isSolved && _inventoryUI.SelectedItem != null)
         {
+            HandleCharacterInteraction(_character);
             _selectedObject = _inventoryUI.SelectedItem;
-            CheckRequiredItems(_selectedObject);
+            Check_requiredItems(_selectedObject);
         }
     }
 
-    private void CheckRequiredItems(string selectedItem)
+    protected override void HandleCharacterInteraction(Character character)
     {
-        foreach (ItemData item in requiredItems)
+        base.HandleCharacterInteraction(character);
+        _myInventory = character.Inventory;
+        _onStartPuzzleAnimation.SetBool("OpenDoor", true);
+    }
+
+    private void Check_requiredItems(string selectedItem)
+    {
+        foreach (ItemData item in _requiredItems)
         {
             if (selectedItem == item.itemName)
             {
-                requiredItems.Remove(item);
+                _requiredItems.Remove(item);
                 _myInventory.RemoveItem(item);
                 break;
             }
         } 
-        if (requiredItems.Count == 0)
+        if (_requiredItems.Count == 0)
         {
             HandlePuzzleFinish();
-            Debug.Log("Finish");
         }
     }
 }
