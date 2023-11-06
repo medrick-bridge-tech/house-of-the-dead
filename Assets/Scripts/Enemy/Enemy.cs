c9e236ffc00f85fd140b57f5f02baea80b7dd4cb
RezaKusha <reza.kusha1@gmail.com>
2023-11-06 17:51:44 +0330

    Create enemy state machine and view cone blueprints

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,112 @@
+﻿using MyStateMachine;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Enemy
+{
+    public class Enemy : MonoBehaviour
+    {
+        [SerializeField] private ViewCone _viewCone;
+        [SerializeField] private NavMeshAgent _agent;
+
+        private StateMachine stateMachine;
+        private Transform detectedTarget;
+        private State searchState, patrolState, distractState, followState;
+        
+        private void Awake()
+        {
+            var enemyStateMachineGraph = new Graph<State>();
+            CreateState();
+            CreateStateMachineGraph();
+            stateMachine = new StateMachine(enemyStateMachineGraph, patrolState);
+            
+            _viewCone.Setup(HandleTargetDetection, HandleTargetLost);
+
+            void CreateState()
+            {
+                searchState = new State(EnterSearchState, () => { }, () => { });
+                patrolState = new State(EnterPatrolState, () => { }, () => { });
+                distractState = new State(EnterDistractState, () => { }, () => { });
+                followState = new State(EnterFollowState, UpdateFollowState, () => { });
+            }
+            
+            void CreateStateMachineGraph()
+            {
+                enemyStateMachineGraph.AddVertex(searchState);
+                enemyStateMachineGraph.AddVertex(patrolState);
+                enemyStateMachineGraph.AddVertex(distractState);
+                enemyStateMachineGraph.AddVertex(followState);
+                
+                enemyStateMachineGraph.AddEdge(patrolState, followState);
+                enemyStateMachineGraph.AddEdge(patrolState, distractState);
+                enemyStateMachineGraph.AddEdge(followState, searchState);
+                enemyStateMachineGraph.AddEdge(searchState, patrolState);
+                enemyStateMachineGraph.AddEdge(searchState, followState);
+                enemyStateMachineGraph.AddEdge(distractState, followState);
+                enemyStateMachineGraph.AddEdge(distractState, patrolState);
+            }
+        }
+
+        private void HandleTargetDetection(GameObject target)
+        {
+            detectedTarget = target.transform;
+            // TODO: Set agent target to target
+        }
+
+        private void HandleTargetLost()
+        {
+            detectedTarget = null;
+            // TODO: Move to search state
+        }
+        
+        void EnterPatrolState()
+        {
+            
+        }
+        
+        void EnterSearchState()
+        {
+            
+        }
+        
+        void EnterDistractState()
+        {
+            
+        }
+        
+        void EnterFollowState()
+        {
+            
+        }
+        
+        void UpdateFollowState()
+        {
+            
+        }
+        
+        void UpdatePatrolState()
+        {
+            
+        }
+        
+        void UpdateSearchState()
+        {
+            
+        }
+
+        void ExitPatrolState()
+        {
+            
+        }
+        
+        void ExitDistractState()
+        {
+            
+        }
+        
+        void ExitSearchState()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/ViewCone.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+namespace Enemy
+{
+    public class ViewCone : MonoBehaviour
+    {
+        [SerializeField] private LayerMask targetLayer;
+        [SerializeField] [Range(.5f, 5f)] private float range = 1;
+        [SerializeField] [Range(0f, 180f)] private float viewDegree = 135f;
+
+        private Action<GameObject> onTargetDeteted;
+        private Action onTargetLost;
+
+        private bool isDetectingTarget;
+        
+        public void Setup(Action<GameObject> targetDetectedCallback, Action targetLostCallback)
+        {
+            onTargetDeteted += targetDetectedCallback;
+            onTargetLost += targetLostCallback;
+        }
+        
+        private void Update()
+        {
+            if (isDetectingTarget == false)
+            {
+                // Raycast multiple rays every 10 degrees
+                // If found target layer
+                onTargetDeteted.Invoke(null);
+                isDetectingTarget = true;
+            }
+            else
+            {
+                // Raycast multiple rays
+                // If lost target
+                onTargetLost.Invoke();
+                isDetectingTarget = false;
+            }
+        }
+    }
+}
